5351c3da2dc2ac8a165cde03534e50a183c4f8f0
Sebastián <sebasmoraresu@gmail.com>
2024-07-04 18:39:40 -0500

    :sparkles:

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,7 @@ import { AuthButtonServer } from '@/app/components/auth-button-server'
 export default function Login () {
   return (
     <section className="grid place-content-center min-h-screen">
-      <h1 className="text-xl font-bold mb-4">Inicia sesión en DevTer</h1>
+      <h1 className="text-xl font-bold mb-4">Inicia sesión en XUTP</h1>
       <AuthButtonServer />
     </section>
   )
